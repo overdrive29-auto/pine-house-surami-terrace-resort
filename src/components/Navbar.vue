f0186049347422fed64d203ec10e254c905ddcd4
Grrah147 <nikoloz.u@gmail.com>
2026-02-07 03:20:24 +0400

    Updated svg logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,95 +5,71 @@
                 <!-- <router-link to="/" activeClass="" exactActiveClass="">
                     <span class="text-2xl permanent-marker-regular px-3">Pine House & Surami Terrace Resort</span>
                 </router-link> -->
-<svg
-class="m-4"
-   width="61.999245"
-   height="26"
-   viewBox="0 0 61.999245 26"
-   version="1.1"
-   id="svg1"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg">
-  <defs
-     id="defs1" />
-  <g
-     id="layer1"
-     transform="translate(-1.0007566,-19)">
-    <path
-       id="path1"
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 62.250034,44.25 C 59.16669,36.083287 56.083345,27.916575 52.999999,19.749863 49.916655,27.916575 46.833312,36.083288 43.749966,44.25 c 6.166689,0 12.333379,0 18.500068,0 z" />
-    <path
-       id="path1-5"
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 41.362664,44.25 C 38.279319,36.083287 35.195975,27.916575 32.11263,19.749862 29.029286,27.916575 25.945941,36.083287 22.862597,44.25 c 6.166689,0 12.333378,0 18.500067,0 z" />
-    <path
-       id="rect1"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 51.5,28.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
-    <path
-       id="rect1-3"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 30.5,28.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
-    <path
-       id="rect1-0"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 54.5,36.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
-    <path
-       id="rect1-0-5"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 48.5,36.5 c 0,2.666667 0,5.333333 0,8 0.833333,0 1.666667,0 2.5,0 0,-2.666667 0,-5.333333 0,-8 -0.833333,0 -1.666667,0 -2.5,0 z" />
-    <path
-       id="rect1-0-5-1"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 34,36.5 c 0,2.666667 0,5.333333 0,8 0.833333,0 1.666667,0 2.5,0 0,-2.666667 0,-5.333333 0,-8 -0.833333,0 -1.666667,0 -2.5,0 z" />
-    <path
-       id="rect1-0-7"
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 27.5,36.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 48.752509,31 c -4.130795,0 -8.261589,0 -12.392384,0"
-       id="path2" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 42.5,31 c 0,4.666667 0,9.333334 0,14.000001"
-       id="path3" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="m 13,20 c 0,8.333333 0,16.666667 0,25"
-       id="path4" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 5.8873005,34.0805 C 9.0214605,34.266377 12.311743,32.15053 13,29"
-       id="path6" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 20.1127,34.0805 C 16.97854,34.266376 13.688258,32.15053 13,29"
-       id="path6-4" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 23.935666,40.249481 C 19.293333,40.472828 13.997069,37.903493 12.981037,33.008568"
-       id="path7-7" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 1.032388,40.496948 C 5.9572825,40.704841 11.799594,38.239329 13,33"
-       id="path7-7-2" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 8.935601,28.128806 C 12.584592,27.403383 12.855611,22.966325 13,20"
-       id="path8" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2;stroke-dasharray:none"
-       d="M 17.0644,28.128797 C 13.415402,27.403389 13.144392,22.96632 13,20"
-       id="path8-4" />
-    <path
-       style="fill:none;stroke:#000000;stroke-width:1.5;stroke-linejoin:round;stroke-miterlimit:2"
-       d="m 41.362664,44.25 c 0.795767,0 1.591535,0 2.387302,0"
-       id="path5" />
-  </g>
-</svg>
-
+                <svg
+                id="logo"
+                class="m-2"
+                width="62"
+                height="26"
+                viewBox="0 0 62 26"
+                xmlns="http://www.w3.org/2000/svg">
+                <g style="fill:none;stroke:currentColor">
+                    <path
+                    style="stroke-width:1.5;"
+                    d="M 61.249655,25.25 C 58.166311,17.083287 55.082966,8.9165749 51.99962,0.74986287 48.916276,8.9165749 45.832933,17.083288 42.749587,25.25 c 6.166689,0 12.333379,0 18.500068,0 z" />
+                    <path
+                    style="stroke-width:1.5;"
+                    d="M 40.362285,25.25 C 37.27894,17.083287 34.195596,8.9165749 31.112251,0.74986187 28.028907,8.9165749 24.945562,17.083287 21.862218,25.25 c 6.166689,0 12.333378,0 18.500067,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 50.499621,9.4999999 c 0,1.0000001 0,2.0000001 0,3.0000001 1,0 2,0 3,0 0,-1 0,-2 0,-3.0000001 -1,0 -2,0 -3,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 29.499621,9.4999999 c 0,1.0000001 0,2.0000001 0,3.0000001 1,0 2,0 3,0 0,-1 0,-2 0,-3.0000001 -1,0 -2,0 -3,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 53.499621,17.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 47.499621,17.5 c 0,2.666667 0,5.333333 0,8 0.833333,0 1.666667,0 2.5,0 0,-2.666667 0,-5.333333 0,-8 -0.833333,0 -1.666667,0 -2.5,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 32.999621,17.5 c 0,2.666667 0,5.333333 0,8 0.833333,0 1.666667,0 2.5,0 0,-2.666667 0,-5.333333 0,-8 -0.833333,0 -1.666667,0 -2.5,0 z" />
+                    <path
+                    style="stroke-width:1;"
+                    d="m 26.499621,17.5 c 0,1 0,2 0,3 1,0 2,0 3,0 0,-1 0,-2 0,-3 -1,0 -2,0 -3,0 z" />
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 47.75213,12 c -4.130795,0 -8.261589,0 -12.392384,0"/>
+                    <path
+                    style="stroke-width:1;"
+                    d="m 41.499621,12 c 0,4.666667 0,9.333334 0,14.000001"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 11.999621,0.99999987 c 0,8.33333303 0,16.66666713 0,25.00000013"
+                    id="path4" />
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 4.8869219,15.0805 c 3.13416,0.185877 6.4244421,-1.92997 7.1126991,-5.0805001"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 19.112321,15.0805 c -3.13416,0.185876 -6.424442,-1.92997 -7.1127,-5.0805001"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="M 22.935287,21.249481 C 18.292954,21.472828 12.99669,18.903493 11.980658,14.008568"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="M 0.03200941,21.496948 C 4.9569039,21.704841 10.799215,19.239329 11.999621,14"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="M 7.9352224,9.1288059 C 11.584213,8.4033829 11.855232,3.9663249 11.999621,0.99999987"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 16.064021,9.1287969 c -3.648998,-0.725408 -3.920008,-5.162477 -4.0644,-8.12879703"/>
+                    <path
+                    style="stroke-width:1.5;"
+                    d="m 40.362285,25.25 c 0.795767,0 1.591535,0 2.387302,0"/>
+                </g>
+                </svg>
             </template>
             <template #item="{ item, props, hasSubmenu, root }">
                 <router-link v-if="item.route" v-slot="{ href, navigate, isActive, isExactActive }" :to="item.route"
@@ -214,7 +190,15 @@ const selectLanguage = (code) => { locale.value = code };
     & .router-link-exact-active {
         opacity: 1;
         color: var(--p-menubar-item-focus-color);
-        background: var(--p-menubar-item-focus-background);
+        background: var(--p-primary-200);
+        border-radius: var(--p-menubar-base-item-border-radius);
+    }
+
+    .phstr-app-dark   & .router-link-active,
+    .phstr-app-dark & .router-link-exact-active {
+        opacity: 1;
+        color: var(--p-menubar-item-focus-color);
+        background: var(--p-primary-800);
         border-radius: var(--p-menubar-base-item-border-radius);
     }
 
@@ -245,8 +229,12 @@ const selectLanguage = (code) => { locale.value = code };
     padding-bottom: 0.75rem;
 }
 
-#svg1 {
-  width: 120px;      /* Scale to 200px wide */
-  height: auto;      /* Maintain aspect ratio */
+#logo {
+  width: 125px;
+  height: auto;
+}
+
+.phstr-app-dark #logo {
+  color: white;
 }
 </style>
