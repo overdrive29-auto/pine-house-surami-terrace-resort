aac9bcd0560a3a8fa0311b3e576ed66d2b859128
Grrah147 <nikoloz.u@gmail.com>
2026-02-16 22:03:29 +0400

    Fixed home layout gap issue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
 	<div id="footer">
-		<div class="phastr-container flex flex-1 w-full justify-center p-4!">
+		<div
+			class="phastr-container flex flex-1 w-full justify-center bg-surface-0 dark:bg-surface-900 p-4!"
+		>
 			<p>{{ $t("footer.copyright") }}</p>
 		</div>
 	</div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
 	<div id="home">
 		<div
-			class="phastr-container grid gap-1 w-full min-h-0 grid-cols-1 lg:grid-cols-[1fr_2fr] bg-linear-to-br from-lime-600 to-lime-100 dark:from-lime-600 dark:to-lime-200"
+			class="phastr-container grid gap-1 lg:gap-px w-full min-h-0 grid-cols-1 lg:grid-cols-[1fr_2fr] bg-linear-to-br from-lime-600 to-lime-100 dark:from-lime-600 dark:to-lime-200"
 		>
-			<div class="grid grid-rows-[1fr_auto] gap-1 min-h-0">
+			<div class="grid grid-rows-[1fr_auto] gap-1 lg:gap-px min-h-0">
 				<IntroSection></IntroSection>
 				<GallerySection></GallerySection>
 			</div>
 
-			<div class="grid grid-rows-[1fr_auto] gap-1 min-h-0">
+			<div class="grid grid-rows-[1fr_auto] gap-1 lg:gap-px min-h-0">
 				<AboutSection></AboutSection>
-				<div class="grid grid-cols-1 lg:grid-cols-2 gap-1">
+				<div class="grid grid-cols-1 lg:grid-cols-2 gap-1 lg:gap-px">
 					<BookingSection @openBookingMenu="handleOpenBookingMenu"></BookingSection>
 					<ContactSection></ContactSection>
 				</div>
